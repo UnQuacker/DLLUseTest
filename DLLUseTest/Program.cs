8d63b81c4f9521d99f2796760a31e4ed6e3c5734
Talgat Sovetbekov <sarkytbayevquacker@gmail.com>
2022-06-20 14:51:04 +0600

    Add project files.

---
+++ DLLUseTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DLLUseTest
+{
+    class Program
+    {
+        [DllImport("DLLTest.dll")]
+        public static extern IntPtr Create();
+
+        [DllImport("DLLTest.dll")]
+        public static extern void TestWrite(IntPtr dll, string string1, string string2, StringBuilder newString);
+        static void Main(string[] args)
+        {
+            IntPtr test = Create();
+            string string1 = "1st string ";
+            string string2 = "2nd string";
+            StringBuilder newString = new StringBuilder();
+            TestWrite(test, string1, string2, newString);
+            Console.WriteLine(newString.ToString());
+        }
+    }
+}
